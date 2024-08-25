eb09cb7174c6a4fe58654181f9486adcff30041b
whywhy <84124699+loshanparajuli@users.noreply.github.com>
2024-08-26 00:46:11 +0545

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import 'react-multi-carousel/lib/styles.css';
 import 'rc-drawer/assets/index.css';
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className={inter.className}>
+      {children} 
+      <Analytics /> 
+      </body>
+      </html>
+   
   )
 }
